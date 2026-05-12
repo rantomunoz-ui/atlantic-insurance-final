c4182aa4593dd1be14a04fee7d8f4ac74515e652
rantomunoz-ui <rantomunoz@gmail.com>
2026-05-11 21:00:12 -0600

    Update index.ts

--- app/pdf/index.ts
+++ app/pdf/index.ts
@@ -1,3 +1,27 @@
+import { generateClientDataForm } from "./clientDataForm";
+import { generateProposalForm } from "./proposalForm";
+import { generatePolicySchedule } from "./policySchedule";
+import { generateCertificateOfInsurance } from "./certificateOfInsurance";
+
 export function generatePdf(title: string, data: any) {
-  console.log(title, data);
+  switch (title) {
+    case "Client Data Form":
+      generateClientDataForm(data);
+      break;
+
+    case "Proposal Form":
+      generateProposalForm(data);
+      break;
+
+    case "Policy Schedule":
+      generatePolicySchedule(data);
+      break;
+
+    case "Certificate of Insurance":
+      generateCertificateOfInsurance(data);
+      break;
+
+    default:
+      console.log("Unknown document:", title);
+  }
 }
