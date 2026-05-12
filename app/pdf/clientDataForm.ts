bcb2fba83efd8084b04a16f10f6b7531e69334aa
rantomunoz-ui <rantomunoz@gmail.com>
2026-05-11 21:03:38 -0600

    Update clientDataForm.ts

--- app/pdf/clientDataForm.ts
+++ app/pdf/clientDataForm.ts
@@ -1,1 +1,7 @@
-export function generateClientDataForm(data:any) { console.log("Client Data Form", data); }
+import { jsPDF } from "jspdf";
+
+export function generateClientDataForm(data: any) {
+  const doc = new jsPDF();
+  doc.text("Client Data Form", 20, 20);
+  doc.save("client-data-form.pdf");
+}
