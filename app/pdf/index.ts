5122a4624733d5cf7de940da22887b3b1cda1038
rantomunoz-ui <rantomunoz@gmail.com>
2026-05-11 20:58:14 -0600

    Update index.ts

--- app/pdf/index.ts
+++ app/pdf/index.ts
@@ -1,4 +1,3 @@
-export { generateClientDataForm } from "./clientDataForm";
-export { generateProposalForm } from "./proposalForm";
-export { generatePolicySchedule } from "./policySchedule";
-export { generateCertificateOfInsurance } from "./certificateOfInsurance";
+export function generatePdf(title: string, data: any) {
+  console.log(title, data);
+}
